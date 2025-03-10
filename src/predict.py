accfe251b8beb1047e85e9e7d8f95d0b816d92bf
Mathman123-cloud <wpradab@ucentral.edu.co>
2025-03-09 19:33:45 -0500

    test

---
+++ src/predict.py
@@ -0,0 +1,3 @@
+from train import entrenamiento
+
+entrenamiento(data).train()
---
+++ src/train.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+
+def training(data):
+    print(data)
+class entrenamiento():
+    def _init_(data):
+        self.data = data
+    def train():
+        print(self.data)
